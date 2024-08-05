8f45a7d4bb633a23a623dcb98e446cc78718b4a5
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-08-05 00:31:05 -0300

    Add project files.

---
+++ AppLiveYoutube/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppLiveYoutube
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppLiveYoutube/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppLiveYoutube
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
